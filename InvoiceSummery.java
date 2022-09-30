f84d7020f32506c95f64802864dd08a1fdc11e3c
radheashu <ashwinihanwate1020@gmail.com>
2022-09-30 18:30:30 +0530

    added:NewPrograms

---
+++ InvoiceSummery.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class InvoiceSummery {
+
+        int totalNoRide;
+        int totalFare;
+        double averageFarePerRide;
+
+        public InvoiceSummary(int totalNoRide, int totalFare) {
+            this.totalNoRide = totalNoRide;
+            this.totalFare = totalFare;
+            this.averageFarePerRide = this.totalFare / this.totalNoRide;
+        }
+
+        public int getTotalNoRide() {
+            return totalNoRide;
+        }
+
+        public int getTotalFare() {
+            return totalFare;
+        }
+
+        public double getAverageFarePerRide() {
+            return averageFarePerRide;
+        }
+    }
+
